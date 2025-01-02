0e2354a6ca30d6bf947544662284cf2df76eb824
brian d foy <briandfoy@pobox.com>
2025-01-02 16:31:22 -0500

    Update copyright

--- lib/Pod/PseudoPod/OReilly.pm
+++ lib/Pod/PseudoPod/OReilly.pm
@@ -65,7 +65,7 @@ brian d foy, C<< <bdfoy@cpan.org> >>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright © 2009-2023, brian d foy <bdfoy@cpan.org>. All rights reserved.
+Copyright © 2009-2025, brian d foy <bdfoy@cpan.org>. All rights reserved.
 
 You may redistribute this under the same terms as Perl itself.
 
